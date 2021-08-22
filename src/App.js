d77f97c19ada1442262328dc448abea5643adc25
MadhuriTeli <madhuriteli07@gmail.com>
2021-08-22 23:23:58 +0530

    user and vendor registraion template

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+//import Register from './components/Register';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+import Login from './components/Login';
+import UserRegistration from './components/UserRegistration';
+import VendorRegistration from './components/VendorRegistration';
+
+function App() {
+    return (
+        <Router>
+            <Switch>
+                {/* <Route path="/" exact component={Register} /> */}
+                <Route path="/login" component={Login} />
+                 <Route path="/" exact component={UserRegistration} />
+                <Route path="/VendorRegistration" component={VendorRegistration} />
+            </Switch>
+        </Router>
+
+        // <div>
+        //     <Register />
+        // </div>
+    );
+}
+
+export default App;
