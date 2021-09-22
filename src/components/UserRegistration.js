53181d8dd46b2e4bdd5bf64542760dd7edebd6bf
MadhuriTeli <madhuriteli07@gmail.com>
2021-09-22 10:43:14 +0530

    integration

--- src/components/UserRegistration.js
+++ src/components/UserRegistration.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
@@ -7,9 +7,14 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-// import { Link } from 'react-router-dom';
-import Box from "@material-ui/core/Box";
 import Register from "./Register";
+import Box from "@material-ui/core/Box";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import DatePicker from "@mui/lab/DatePicker";
+//const baseURL = "http://localhost:8080/user/sign-up";
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
@@ -45,6 +50,56 @@ const useStyles = makeStyles((theme) => ({
 export default function UserRegistration() {
   const classes = useStyles();
 
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [address, setAddress] = useState("");
+  const [mobno, setMobno] = useState("");
+  const [dob, setDob] = useState(new Date());
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+  // async function UserSignUp() {
+  //   let item = { fname, lname, address, mobno, dob, email, password };
+  //   console.warn(item);
+  //   let result = await fetch("http://localhost:8080/user/sign-up", {
+  //     method: "POST",
+  //     body: JSON.stringify(item),
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Accept: "application/json",
+  //     },
+  //   });
+  //   result = await result.json();
+  //   localStorage.setItem("user-info", JSON.stringify(result));
+  //   history.push("/userLogin");
+  // }
+
+  // const [post, setPost] = React.useState(null);
+  // React.useEffect(() => {
+  //   axios.get(`${baseURL}`).then((response) => {
+  //     setPost(response.data);
+  //   });
+  // }, []);
+
+  const Registerfun = () => {
+    axios
+      .post("http://localhost:8080/user/sign-up", {
+        fname: fname,
+        lname: lname,
+        address: address,
+        mobno: mobno,
+        dob: dob,
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        // setPost(response.data);
+        localStorage.setItem("user-info", JSON.stringify(response.data));
+        history.push("/userLogin");
+        console.log(response.data);
+      });
+  };
+  //if (!post) return "No post!";
   return (
     <div>
       <Register />
@@ -57,13 +112,15 @@ export default function UserRegistration() {
               <Grid item xs={12} sm={6}>
                 <TextField
                   autoComplete="fname"
-                  name="firstName"
+                  name="fname"
                   variant="outlined"
                   required
                   fullWidth
-                  id="firstName"
+                  id="fname"
                   label="First Name"
                   autoFocus
+                  value={fname}
+                  onChange={(e) => setFname(e.target.value)}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -71,10 +128,12 @@ export default function UserRegistration() {
                   variant="outlined"
                   required
                   fullWidth
-                  id="lastName"
+                  id="lname"
                   label="Last Name"
-                  name="lastName"
+                  name="lname"
                   autoComplete="lname"
+                  value={lname}
+                  onChange={(e) => setLname(e.target.value)}
                 />
               </Grid>
 
@@ -87,6 +146,8 @@ export default function UserRegistration() {
                   label="Address"
                   name="address"
                   autoComplete="address"
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
                 />
               </Grid>
 
@@ -95,22 +156,31 @@ export default function UserRegistration() {
                   variant="outlined"
                   required
                   fullWidth
-                  id="phone"
+                  id="mobno"
                   label="Phone Number"
-                  name="phone"
+                  name="mobno"
                   autoComplete="phone"
+                  value={mobno}
+                  onChange={(e) => setMobno(e.target.value)}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  id="dob"
-                  label="Date of Birth"
-                  name="dob"
-                  autoComplete="dob"
-                />
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <DatePicker
+                    variant="outlined"
+                    required
+                    id="dob"
+                    label="Date of Birth"
+                    name="dob"
+                    type="date"
+                    autoComplete="dob"
+                    value={dob}
+                    onChange={(newValue) => setDob(newValue)}
+                    renderInput={(params) => (
+                      <TextField variant="outlined" {...params} />
+                    )}
+                  />
+                </LocalizationProvider>
               </Grid>
 
               <Grid item xs={12}>
@@ -122,6 +192,8 @@ export default function UserRegistration() {
                   label="Email Address"
                   name="email"
                   autoComplete="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -134,6 +206,8 @@ export default function UserRegistration() {
                   type="password"
                   id="password"
                   autoComplete="current-password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </Grid>
             </Grid>
@@ -143,6 +217,7 @@ export default function UserRegistration() {
               variant="contained"
               color="primary"
               className={classes.submit}
+              onClick={Registerfun}
             >
               Register
             </Button>
